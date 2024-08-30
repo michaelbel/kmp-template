03b7c4c24e9119aaba729f49d2c565a0f0acbe00
michaelbel <michael-bel@outlook.com>
2024-08-30 15:24:01 +0300

    Update project

--- composeApp/src/iosMain/kotlin/org/michaelbel/template/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/michaelbel/template/MainViewController.kt
@@ -1,5 +1,6 @@
-package org.michaelbel.kmptemplate
+package org.michaelbel.template
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.michaelbel.kmptemplate.App
 
 fun MainViewController() = ComposeUIViewController { App() }
