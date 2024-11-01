12f92c301fbaf748133805e462bc06694e524040
michaelbel <michael-bel@outlook.com>
2024-11-01 21:04:53 +0300

    Update project

--- composeApp/src/commonMain/kotlin/org/michaelbel/template/App.kt
+++ composeApp/src/commonMain/kotlin/org/michaelbel/template/App.kt
@@ -1,17 +1,26 @@
 package org.michaelbel.template
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun App() {
     MaterialTheme {
-        Button(
-            onClick = {}
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            Text("Print Log")
+            Button(
+                onClick = {}
+            ) {
+                Text("Print Log")
+            }
         }
     }
 }
